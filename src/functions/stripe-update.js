b2514a85d88614e19a2a231ef95f94afb8210144
Ian C <ian@thecommittedbug.io>
2020-09-15 22:22:30 -0400

    add update serverless function

---
+++ src/functions/stripe-update.js
@@ -0,0 +1,28 @@
+const Stripe = require("stripe")
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY)
+
+// TODO: Add functionality to fully update a product despite Stripe's update limitation to 'metadata', 'nickname', & 'active' fields
+module.exports.handler = async (event, context, callback) => {
+  try {
+    const requestBody = JSON.parse(event.body)
+    const { id, object } = requestBody
+
+    var updatedObject
+    if (id.substr(0, 3) === "pri") {
+      updatedObject = await stripe.prices.update(id, { ...object })
+    } else if (id.substr(0, 3) === "pro") {
+      updatedObject = await stripe.products.update(id, { ...object })
+    }
+
+    callback(null, {
+      statusCode: 201,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ updatedObject }),
+    })
+  } catch (error) {
+    callback(error.message)
+  }
+}
