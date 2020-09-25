201bf67a5c52efa7d2b74d9e4c46776b76b52cf4
Ian C <ian@thecommittedbug.io>
2020-09-25 16:43:01 -0400

    add missing json header

--- src/functions/stripe-read.js
+++ src/functions/stripe-read.js
@@ -14,6 +14,7 @@ exports.handler = async (event, context, callback) => {
     statusCode: 200,
     headers: {
       "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json"
     },
     body: JSON.stringify({ prices })
   }
