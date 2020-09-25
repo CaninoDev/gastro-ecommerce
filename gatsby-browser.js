62ce32fbd2cbf2da4f2fdca08346909697a8cb52
Ian C <ian@thecommittedbug.io>
2020-09-25 16:38:37 -0400

    wrap with Stripe's Elements

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,15 @@
  */
 
 // You can delete this file if you're not using it
+
+import React from "react"
+import { loadStripe } from "@stripe/stripe-js"
+import { Elements } from "@stripe/react-stripe-js"
+
+const stripePromise = loadStripe(
+  "pk_test_51Gsyi3GuPXrd5SADNu0p4FJcKGE7oTfWuemre8fvW0Zr4whA1dZzsFMBL42EStX6h7XoHL0DbKpXwAZVwV3HiNLM00s5Rt739H"
+)
+
+export const wrapRootElement = ({ element }) => (
+  <Elements stripe={stripePromise}>{element}</Elements>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,37 +1,26 @@
 import React from "react"
-import { Elements } from "@stripe/react-stripe-js"
-import { loadStripe } from "@stripe/stripe-js"
 
 import Layout from "../components/layout"
 import ProductsProvider from "../components/products-provider"
 import CartProvider from "../components/cart-provider"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const stripePromise = loadStripe(process.env.STRIPE_PUBLISHABLE_KEY)
-
 const IndexPage = () => (
-  <Elements stripe={stripePromise}>
-    <Layout>
-      <ProductsProvider>
-        <CartProvider>
-          <SEO
-            title="Home"
-            keywords={[
-              `gatsby`,
-              `application`,
-              `e-commerce`,
-              `restaurant`,
-              `stripe`,
-            ]}
-          />
-          <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-            <Image />
-          </div>
-        </CartProvider>
-      </ProductsProvider>
-    </Layout>
-  </Elements>
+  <ProductsProvider>
+    <CartProvider>
+      <SEO
+        title="Home"
+        keywords={[
+          `gatsby`,
+          `application`,
+          `e-commerce`,
+          `restaurant`,
+          `stripe`,
+        ]}
+      />
+      <Layout />
+    </CartProvider>
+  </ProductsProvider>
 )
 
 export default IndexPage
